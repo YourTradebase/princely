81a3edccd7022308afbfebeb36cd142a4fc329c1
Habib Alamin <habib@yourtradebase.com>
2016-12-14 13:42:14 +0000

    Fix stylesheets path being derived from non-existent asset config option
    [Fixes #136029523]

--- lib/princely/asset_support.rb
+++ lib/princely/asset_support.rb
@@ -15,9 +15,17 @@ module Princely
     end
 
     def asset_file_path(asset)
-      # Remove /assets/ from generated names and try and find a matching asset
-      Rails.application.assets ||= Sprockets::Environment.new
-      Rails.application.assets.find_asset(asset.gsub(%r{/assets/}, "")).try(:pathname) || asset
+      asset = asset.gsub(%r'/assets/', '')
+
+      if Rails.application.assets
+        Rails.application.assets.find_asset(asset).try(:pathname) || asset
+      else
+        path = view_context.asset_path(asset.concat('.css')) # asset filename, inc. hash, with relative path
+          .scan(%r"#{Regexp.escape(asset.split('.').first)}[-0-9a-f]*?\.[a-z]+?$")
+          .first
+
+        Rails.root.join('public/assets', path).to_s
+      end
     end
   end
 end
